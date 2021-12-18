c9e15e4f054ab193f6fe617afd315195ba5b83ee
igorbr <igor.bronovytskyi@playtech.com>
2021-12-18 10:50:29 +0200

    check project: http

--- http/js/http.js
+++ http/js/http.js
@@ -1,15 +1,13 @@
 // Write your code here
 
 $('.add-games').on('click', function (e) {
-    e.preventDefault();
     $.ajax({
         method: 'GET',
         url: 'http://127.0.0.1:3000/games',
         dataType: 'json',
     }).done(function(games){
-        console.log(games)
         var gameTemplate = document.querySelector('#game-template').innerHTML;
-        document.querySelector('#games').innerHTML += doT.template(gameTemplate)({games: games});
+        document.querySelector('#games').innerHTML = doT.template(gameTemplate)({games: games});
     })
 });
 
@@ -19,7 +17,6 @@ $('.btn-nav-en').on('click', function () {
         url: 'http://127.0.0.1:3000/games/menu/en',
         dataType: 'json',
     }).done(function(navEn){
-        console.log(navEn)
         var navTemplate = document.querySelector('#nav-template').innerHTML;
         document.querySelector('#nav').innerHTML = doT.template(navTemplate)({nav: navEn});
     })
@@ -31,7 +28,6 @@ $('.btn-nav-ru').on('click', function () {
         url: 'http://127.0.0.1:3000/games/menu/ru',
         dataType: 'json',
     }).done(function(navRu){
-        console.log(navRu)
         var navTemplate = document.querySelector('#nav-template').innerHTML;
         document.querySelector('#nav').innerHTML = doT.template(navTemplate)({nav: navRu});
     })
@@ -43,9 +39,8 @@ $('.add-whores').on('click', function () {
         url: 'http://127.0.0.1:3000/games/whores',
         dataType: 'json',
     }).done(function(whores){
-        console.log(whores)
         var whoresTemplate = document.querySelector('#whore-template').innerHTML;
-        document.querySelector('#whores').innerHTML += doT.template(whoresTemplate)({whores: whores});
+        document.querySelector('#whores').innerHTML = doT.template(whoresTemplate)({whores: whores});
     })
 });
 
@@ -55,9 +50,9 @@ $('.add-movies').on('click', function () {
         url: 'http://127.0.0.1:3000/games/movies',
         dataType: 'json',
     }).done(function(movies){
-        console.log(movies)
         var movieTemplate = document.querySelector('#movie-template').innerHTML;
-        document.querySelector('#movies').innerHTML += doT.template(movieTemplate)({movies: movies});
+        document.querySelector('#movies').innerHTML = doT.template(movieTemplate)({movies: movies});
     })
 });
 
+//TODO: XMLHttpRequest
